b441f0f9e830d715b31234d66d4d08b7e02c10fb
John <fuchiang137@gmail.com>
2022-05-21 08:13:47 +0000

    feat: server multi connection, client req model

---
+++ client.py
@@ -0,0 +1,19 @@
+import zmq
+import pandas as pd
+
+df = pd.DataFrame({
+    "ip": "127.0.0.1",
+    "port": 38010,
+    "cpu": 30, 
+    "connectivity": 60
+}, index=[0])
+
+# Estblish Connection
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect ("tcp://127.0.0.1:7788")
+
+socket.send_pyobj(df)
+print(socket.recv_pyobj())
+
+# 
